668148f18e758c974d7589ed14499c2b55cb62b3
Muhammad Adnan <nandaswati8123796@gmail.com>
2022-02-21 01:03:27 -0800

    first_dart_practice

---
+++ bin/my_app.dart
@@ -0,0 +1,290 @@
+void main(List<String> arguments) {
+// int val1 = 23;
+// int val2 = 50;
+// print (val1+val2);
+// print (val1-val2);
+// print (val1*val2);
+// print (val1/val2);
+// print (val2~/val1);
+//
+// print (val1.isEven);
+// print (val2.isEven);
+// print (val1.isOdd);
+// print (val2.isOdd);
+//
+// print (val1.toString());
+// val1 = val2;
+//
+// String name = "My Name is Adnan";
+// print (name.isEmpty);
+// print (name.isNotEmpty);
+//
+// double number = 3;
+// String latter = "Adnan";
+// print (latter.toLowerCase());
+// var mix = number+ val2;
+// print (mix);
+//
+// var number1 = latter.toUpperCase();
+// print (number1);
+//
+// var num3 = latter.contains("M.Adnan");
+// print (num3);
+//
+// var value1 = "My Name is Muhammad Adnan";
+// var value2 = value1.length;
+// print (value2);
+// var value4 = "Muhammad_adnan10";
+// var value5 = value4.replaceAll("a","m");
+// print (value5);
+//
+// var value6 = value4.replaceRange(1, 3, "b");
+// print (value6);
+//
+// print (value4.substring(0));
+// print (value4.split("_"));
+//
+//
+// String abc = "My Name Is Muhammad Adnan";
+// var abcd = abc.indexOf("Muhammad");
+// print (abcd);
+// var abc2 = abc.indexOf("Muhammad.");
+// print (abc2);
+// print (abc.isEmpty);
+// print (abc.isNotEmpty);
+
+// double a = 200.5;
+// int b = 20;
+// print ("Addition: ${a+b}");
+// print ("Multiplication: ${a*b}");
+// print ("Division:${b / a}");
+// print ("Subtraction:${b - a}");
+// print ("Integer PArt of Division:${b ~/ a}");
+// print ("Remainder:${a % b}");
+
+// int a1 = 19;
+// int a2 = 5;
+// int a3 = 10;
+// print ("Addition: ${--a1 + a2}");
+//  a1 += a2;
+//  a1++;
+//  ++a1;
+//  print ("Addition:${a1++ + a2}");
+//
+//
+
+// print (a1 > a2);
+// print (a1 < a2);
+// print (a1 > a2 && a1 < a3);
+// print (a1 > a2 && a2 < a3);
+// print (a1 > a2 || a2 < a3);
+// print (a1 < a2 || a3 < a2);
+// print (!(a1 > a2) || a3 > a1);
+// print (a2 != a3);
+// print(a2 == a3);
+//
+
+//  int a1 = 19;
+//  int a2 = 5;
+//  int a3 = 10;
+// //  print (a1 is int);
+// // print (a1 is! int);
+// // print (a1 is! String);
+// // print (a1 is num);
+// // print (a2 is bool);
+//
+// var res = a3 > a1 ? "Greater" : "Smaller";
+// print (res);
+
+// var age = 20;
+//  var  isAdult = age >= 20 ? "True" : "False";
+//  print (isAdult);
+//
+// var  isAdult = age >= 21 ? "Allow" : "Not Allow";
+// print (isAdult);
+//
+// var a = 10 ;
+// var b = 20;
+// var c = b ?? a;
+// print (c);
+
+// int myage = 20;
+// int yourage = 20;
+//
+// if(myage > yourage){
+//   print ("I am elder than you");
+// }else if (myage == yourage){
+//   print ("We both are same age.");
+// }else{
+//   print ("you are elder than me .");
+// }
+// print ("Good Luck");
+//
+
+
+// var val1 = 40 ;
+// var val2 = 50;
+//
+// String message;
+//
+// if(val1 <= val2){
+//   message = "value 1 is greater than value 2";
+// }else if (val1 != val2){
+//   message = "value 1 is not equal to value 2";
+// }else{
+//  message = "value 1 is less than value 2";
+// }
+//   print (message);
+
+// var marks = 0.70;
+//
+// String grade;
+// if (marks >= 0.90){
+//   grade = "A";
+// }else if(marks >= 0.80){
+//   grade = "B";
+// }else if(marks >= 0.70){
+//   grade = "C";
+// }else if (marks >= 0.60){
+//   grade = "D";
+// }else if (marks >= 0.50){
+//   grade = "E";
+// }else if (marks >= 0.40){
+//   grade = "F";
+// }else if (marks < 40){
+//   grade = "G";
+// }else {
+//   grade = "Fail";
+// }
+// print (grade);
+//
+
+
+// var marks = 0.30;
+//
+// String grade;
+// if (marks >= 0.90){
+//   grade = "A";
+// }else if(marks >= 0.80){
+//   grade = "B";
+// }else if(marks >= 0.70){
+//   grade = "C";
+// }else if (marks >= 0.60){
+//   grade = "D";
+// }else if (marks >= 0.50){
+//   grade = "E";
+// }else if (marks >= 0.40){
+//   grade = "F";
+// }else {
+//   grade = "Fail";
+// }
+// print ("Grade = $grade");
+
+//
+// int day = 9;
+// switch(day){
+//   case 1:
+//   print ("Monday");
+//   break;
+//   case 2:
+//   print ("Tuesday");
+//   break;
+//   case 3:
+//   print("Wednesday");
+//   break;
+//   case 4:
+//   print("Thursday");
+//   break;
+//   case 5:
+//   print("Friday");
+//   break;
+//   case 6:
+//   print ("Saturday");
+//   break;
+//   case 7:
+//   print ("Sunday");
+//   break;
+//   default :
+//   print ("Invalid Day!");
+// }
+// print ("Done ");
+
+
+// int day = 3;
+//
+// switch(day){
+//   case 1 :
+//     print ("Monday");
+//     break;
+//   case 2 :
+//     print("Tuesday");
+//     break;
+//   case 3 :
+//     print ("Wednesday");
+//     break;
+//   default :
+//     print ("Invalide Day!");
+//
+// }
+// print ("Done");
+
+// List names = ["Adnan","Zeeshan","Yasir"];
+// int i =0;
+// while(i < names.length){
+//   print (names[i]);
+//   i++;
+// }
+
+// int counter = 1;
+// while (counter <= 10){
+//   print (counter);
+//   counter++;
+// }
+// print ("While loop ends");
+
+  // int name = 2;
+  // int count = 1;
+  // while (count <= 10) {
+  //   print("$name x $count = ${name * count}");
+  //   count++;
+  // }
+
+  // var name = ["Adnan", "Zeeshan", "Pinger", "Ali"];
+  // for (var i = 0; i <= 3; i++) {
+  //   print(name[i]);
+  // }
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+  // print("For Loop ends here!");
+
+  // var name = 3;
+  //
+  // for (var i = 1; i <= 10; i++) {
+  //   print("$name x $i = ${name * i}");
+  // }
+
+  // var name = 3;
+  //
+  // for (var i = 1; i <= 10; i +=4) {
+  //   print("$name x $i = ${name * i}");
+  // }
+
+  // int table_no = 5;
+  // int endsat = 10;
+  // for (int i = 1; i <=endsat; i++){
+  //   print("$table_no x $i = ${table_no * i}");
+  // }
+
+  for(int i = 1; i <= 10; i++){
+    if(i == 5){
+      print ("Exiting Loop");
+      break;
+    }
+    if (i % 3 == 0){
+      print ("$i is an odd number ");
+      continue;
+    }
+    print ("$i is an even number ");
+  }
+}
